568b60319b4337ea93bb7aec0bb187242096d786
Alexandre Bessard <alexandre.bessard@gmail.com>
2023-10-31 14:26:59 -0400

    shopping-edit when adding a new ingredient

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, EventEmitter, Output, ViewChild} from '@angular/core';
+import {Ingredient} from "../../shared/Ingredient.model";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent {
+
+  @ViewChild('nameInput')
+  nameInputRef: ElementRef;
+
+  @ViewChild('amountInput')
+  amountInput: ElementRef;
+
+  @Output()
+  ingredientAdded = new EventEmitter<Ingredient>();
+
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmout = this.amountInput.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmout);
+    this.ingredientAdded.emit(newIngredient);
+  }
 
 }
